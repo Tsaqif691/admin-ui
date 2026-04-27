cdc8f7c307c1cf7c3a73e61026288267641ab0c5
Tsaqif691 <muhammadtsaqifakmal@gmail.com>
2026-04-27 23:47:43 +0700

    Mencoba menghubungkan dan menggunakan data statis dan data API

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserCard from "./UserCard";
+import { getUsers } from "./Services"; 
 
 function Exercise() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const data = await getUsers();
+      setUsers(data);
+    } catch (error) {
+      console.error("[Component] Gagal menampilkan data:", error.message);
+    }
+  };
+  fetchData();
+}, []);
+
   return (
     <>
       <div className="min-h-screen bg-gray-100 p-6">
@@ -9,24 +24,9 @@ function Exercise() {
           User Cards
         </h1>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
-          <UserCard 
-          name="Tsaqif"
-          email="Tsaqif@gmail.com"
-          street="Jl. Merdeka No. 123"
-          city="Jakarta" 
-          />
-          <UserCard 
-          name="Akmal"
-          email="Akmal@gmail.com"
-          street="Jl. Sudirman No. 456"
-          city="Bandung"
-          />
-          <UserCard 
-          name="Ivan"
-          email="Ivan@gmail.com"
-          street="Jl. Diponegoro No. 789"
-          city="Surabaya"
-          />
+          {users.map((user, index) => (
+            <UserCard key={index} {...user} />
+          ))}
         </div>
       </div>
     </>

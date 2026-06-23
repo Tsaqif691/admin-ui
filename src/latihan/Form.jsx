ec7eb5f615041622d11e84a5b6e2a2eeca1728ae
Tsaqif691 <muhammadtsaqifakmal@gmail.com>
2026-06-23 09:02:54 +0700

    fix error vercel

--- src/latihan/Form.jsx
+++ src/latihan/Form.jsx
@@ -1,15 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Form = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [goals, setGoals] = useState({});
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(email);
-    console.log(password);
-    console.log("-");
+
+    try {
+      const response = await axios.post(
+        "https://jwt-auth-eight-neon.vercel.app/login",
+        { email, password },
+      );
+
+      console.log(response.data);
+      localStorage.setItem("token", response.data.refreshToken);
+    } catch (error) {
+      console.error(error);
+    }
   };
+
+  const fetchGoals = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const response = await axios.get(
+        "https://jwt-auth-eight-neon.vercel.app/goals",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+
+      console.log(response.data);
+      setGoals(response.data.data[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchGoals();
+  }, []);
+
+  fetchGoals();
 
   return (
     <div className="p-4">
@@ -36,6 +73,10 @@ const Form = () => {
         <br />
         <input type="submit" value="send" className="bg-gray-200 p-2" />
       </form>
+      <hr className="py-4" />
+      Present amount : {goals.present_amount}
+      <br />
+      Target Amount : {goals.target_amount}
     </div>
   );
 };

b00405bc76c2c45684f43a409e65247f79b5e1c2
Tsaqif691 <muhammadtsaqifakmal@gmail.com>
2026-06-23 08:08:26 +0700

    fix error vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ThemeContextProvider } from "./context/themeContext.jsx";
+import { ThemeContextProvider } from "./Context/themeContext.jsx";
+import Form from "./latihan/Form.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeContextProvider>
-      <App />
-    </ThemeContextProvider>
+    
+      <Form />
+    
   </React.StrictMode>,
 );
